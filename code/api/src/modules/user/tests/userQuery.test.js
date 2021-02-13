a116b36aa5c0772a34d1dd294cd16c85da434dbd
Nick King <nickmaxking@gmail.com>
2021-02-12 18:30:57 -0700

    Add tests for user and userUpdate queries
    Co-authored-by: Connor Ferguson <68167430+cpfergus1@users.noreply.github.com>
    Co-authored-by: Hanna Davis <63931068+Oxalisviolacea@users.noreply.github.com>

--- code/api/src/modules/user/tests/userQuery.test.js
+++ code/api/src/modules/user/tests/userQuery.test.js
@@ -16,50 +16,27 @@ describe('user queries', () => {
       })
     )
   })
-  // it('returns true for true', (done) => {
-  //   expect(true).toBe(true)
-  // done()})
+
+  it('user query', async (done) => {
+    const response = await request(server)
+      .post('/')
+      .send({query: '{user(id: 3){ id name email role style_preference } }'})
+      .expect(200)
+    expect(response.body.data.user.id).toBe(3)
+    expect(response.body.data.user.name).toBe('The Admin')
+    done();
+  })
 
   it('user update mutations', async (done) => {
     const response = await request(server)
       .post('/')
-      .send({mutation: '{userUpdate(id: 1 style_preference: "punk"){ id name email password role style_preference } }'})
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+      .send({
+        query: "mutation {userUpdate(id: 3, style_preference: \"preppy\"){ name style_preference } }"
+      })
       .expect(200)
-    expect(response.body.data.user.id).toBe()
+    expect(response.body.data.userUpdate.style_preference).toBe("preppy")
     done();
   })
 })
-
-  // it('Async test', async done => {
-  //   // Do your async tests here
-  //
-  //   done()
-  // })
-
-//   import request from 'supertest'; // import request library from supertest
-//   import express from 'express'; // import express so we can create a mock server
-//   import graphqlHTTP from 'express-graphql'; // import graphqlHTTP express library
-//   import schema from '../../../setup/schema'; // import our graphql schema
-//   // we create describe functions similar to RSpec
-//
-// describe('user queries', () => {
-//   let server = express();
-//   beforeAll(() => {
-//       server.use(
-//       "/",
-//       graphqlHTTP({
-//         schema: schema,
-//         graphiql: false
-//       })
-//     )
-//   })
-//
-  // it('test description'), async (done) => {
-  //   const response = await request(server)
-  //     .post('/')
-  //     .send({query: '{users {name} }'})
-  //     .expect(200)
-  //   expect(response.body.data.users.length).toBe(2)
-  //   done();
-  // }
-// });
